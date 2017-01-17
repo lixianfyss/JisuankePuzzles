651a667e737b0178a0e16eaa67d9fbade07da70d
Li <Li@G-LAPTOP>
2017-01-18 07:53:36 +0800

    Basic version 0.2

--- gridLineNum/Source.cpp
+++ gridLineNum/Source.cpp
@@ -17,29 +17,40 @@ bool reciprocal(int x, int y)
 	if (y == 0) return false;
 	return true;
 }
-int new_line(int x, int y)
+int new_line(int x, int y, int &sum_lu, int &sum_ld, int &sum_r)
 {
-	int t_sum = 0, mark_j = floor(y / 2.0), mark_k = floor((x + 1) / 2.0);
-	for (int j = 1; j <= mark_j; j++)
+	int t_sum = 0, mark_v = floor(y / 2.0), mark_h = floor((x + 1) / 2.0), border = 0;
+	if (y % 2 == 0)
 	{
-		for (int k = 1; k < mark_k; k++)
+		for (int i = 1; i < mark_h; i++)
 		{
-			if (reciprocal(j, k)) t_sum += k;
+			if (reciprocal(mark_v, i)) sum_lu += i;
+			if (reciprocal(y, i)) sum_ld += x - i;
 		}
-		for (int k = mark_k; k < x; k++)
+		for (int i = mark_h; i < x; i++)
 		{
-			if (reciprocal(j, k)) t_sum += x - k;
+			if (reciprocal(y - 1, i)) sum_r += x - i;	if (reciprocal(y, i)) sum_r += x - i;
 		}
+		t_sum = sum_lu + sum_ld + sum_r;
 	}
-	for (int j = mark_j + 1; j <= y; j++)	for (int k = 1; k < x; k++)
+	else
 	{
-		if (reciprocal(j, k)) t_sum += x - k;
+		for (int i = 1; i < mark_h; i++)
+		{
+			if (reciprocal(mark_v + 1, i)) border += x - i;
+			if (reciprocal(y, i)) sum_ld += x - i;
+		}
+		for (int i = mark_h; i < x; i++)
+		{
+			if (reciprocal(y, i)) t_sum += x - i;
+		}
+		t_sum += sum_lu + sum_ld + sum_r; sum_ld -= border; border = 0;
 	}
 	return t_sum;
 }
 int main()
 {
-	int n, x, y, sum = 0, temp;
+	int n, x, y, sum = 0, temp, sum_lu = 0, sum_ld = 0, sum_r = 0;
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
@@ -49,8 +60,8 @@ int main()
 		}
 		for (int j = 2; j < y; j++)
 		{
-			sum += (2 * new_line(x, j)) % max;
+			sum += (2 * new_line(x, j, sum_lu, sum_ld, sum_r)) % max;
 		}
-		sum += x*x + y; cout << sum << endl; sum = 0;
+		sum += x*x + y; cout << sum << endl; sum = 0; sum_lu = 0; sum_ld = 0; sum_r = 0;
 	}
 }
